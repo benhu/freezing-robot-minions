d94a25fed3e7d396906f040a5bbcc5c129a86f29
Benjamin Humeau <benjamin@pakitow.eu>
2015-06-15 17:36:59 +0200

    Sa march, c tro swag

--- app/minion.cpp
+++ app/minion.cpp
@@ -6,3 +6,17 @@ Minion::Minion(std::string nom, float txBanana) {
     this->nom = nom;
     this->txBanana= txBanana;
 }
+
+bool Minion::addPts(int pts) {
+    std::set<int>::iterator it;
+
+    it=this->tousLesPointsQuilAvisite.find(pts);
+    
+    if(it == this->tousLesPointsQuilAvisite.end()) {
+        return false;
+    }
+
+    this->tousLesPointsQuilAvisite.insert(pts);
+
+    return true;
+}
