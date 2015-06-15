262bf55270cb059b1ee596fdefb8d055cc121e85
ojbruce <olivia.j.bruce@gmail.com>
2015-06-15 17:52:03 +0200

    Remplify mAIN

--- app/main.cpp
+++ app/main.cpp
@@ -23,14 +23,17 @@ int main() {
     //int nbVille = 29; // en minion
     //int tpsVisite = 30;// en min
     //double evapPheromone = 10;
+    int nbIteration = 0;
 
 
     Graph* graph = new Graph();    
 
+    Minion* lesMinions = new Minion[nbMinion];
+
     for(int nbMimi = 0; nbMimi < nbMinion; ++nbMimi) {
         int firstPt = getRandValue(0, 29);
         Minion* mimi = new Minion("Serge", firstPt);
-
+        lesMinions[nbMimi] = mimi;
     }
 
     /*tant que la condition d'arrêt n'est pas vérifiée :
@@ -42,6 +45,24 @@ int main() {
     biodégradation des bananes
     finilabananaparti
     */
+
+
+
+    while(nbIteration != 100){
+
+        for(int nbMimi = 0; nbMimi < nbMinion; ++nbMimi){
+            /*pour chaque evenement non visité i :
+                choisir une evenement dans la liste des evenements restants
+            déposer des bananes sur le trajet*/
+
+        }
+
+        //eVAPORATION DE LA BANANE
+        
+        nbIteration++;
+    }
+
+
 
     return 0;
 }
